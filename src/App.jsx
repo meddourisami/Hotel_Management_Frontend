8c6aacd7cbfdec99d0e5bcdc752fc5ad687a7913
meddourisami <sami.meddouri@etudiant-fst.utm.tn>
2024-06-30 03:54:35 +0200

    setting up the router

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,23 @@ import "/node_modules/bootstrap/dist/js/bootstrap.min.js"
 import './App.css'
 import AddRoom from './components/room/AddRoom'
 import ExistingRooms from "./components/room/ExistingRooms"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import EditRoom from "./components/room/EditRoom"
+import Home from "./components/home/Home"
 
 function App() {
 
   return (
     <>
-      <AddRoom />
-      <ExistingRooms />
+      <main>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home/>} />
+            <Route path="/edit-room/:roomId" element={<EditRoom />} />
+            <Route path="/existing-rooms" element={<ExistingRooms/>} />
+          </Routes>
+        </Router>
+      </main>
     </>
   )
 }
