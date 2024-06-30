9749ef79fc51d30915bd3dc915c6ed041ac19702
meddourisami <sami.meddouri@etudiant-fst.utm.tn>
2024-06-30 18:30:18 +0200

    adding update room functionality

--- src/components/utils/ApiFunctions.js
+++ src/components/utils/ApiFunctions.js
@@ -48,3 +48,23 @@ export async function deleteRoom(roomId) {
         throw new Error(`Error deleting room ${error.message}`)
     }
 }
+
+/* updates a room by id */
+export async function updateRoom(roomId, roomData) {
+    const formData = new FormData()
+    formData.append("roomType", roomData.roomType)
+    formData.append("roomPrice", roomData.roomPrice)
+    formData.append("photo", roomData.photo)
+    const response = await api.put(`/rooms/update/${roomId}`)
+    return response
+}
+
+/* gets a room by id */
+export async function getRoomById(roomId) {
+    try {
+        const result = await api.get(`/rooms/room/${roomId}`)
+        return result.data
+    } catch (error) {
+        throw new Error(`Error fetching room ${error.message}`)
+    }
+ }
