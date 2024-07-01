10038b07163389f98d9724d52b4c1034b0e6d92a
meddourisami <sami.meddouri@etudiant-fst.utm.tn>
2024-07-01 12:58:43 +0200

    added browsing all rooms

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import EditRoom from "./components/room/EditRoom"
 import Home from "./components/home/Home"
 import NavBar from "./components/layout/NavBar"
 import Footer from "./components/layout/Footer"
+import RoomListing from "./components/room/RoomListing"
 
 function App() {
 
@@ -21,7 +22,8 @@ function App() {
             <Route path="/" element={<Home/>} />
             <Route path="/edit-room/:roomId" element={<EditRoom />} />
             <Route path="/existing-rooms" element={<ExistingRooms />} />
-            <Route path="/add-room" element={<AddRoom/>} />
+            <Route path="/add-room" element={<AddRoom />} />
+            <Route path="/browse-all-rooms" element={<RoomListing/>} />
           </Routes>
         </Router>
         <Footer/>
